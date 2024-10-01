ae93452efef61b6161a8ce7c2dd9b2d1e30f358d
Bilal786 <bilalbashir95@gmail.com>
2024-10-01 18:19:27 +0500

    chanegs

--- routes/blog.php
+++ routes/blog.php
@@ -13,11 +13,12 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
 
-    Route::group(['prefix' => 'new-service-pages'], function () {
-        Route::post('get',[ServicePagesController::class,'getPages']);
-        Route::post('validate-slug',[ServicePagesController::class,'validateSlug']);
-        Route::post('add',[ServicePagesController::class,'store']);
-        Route::get('delete-page/{id}',[ServicePagesController::class,'delete_page']);
-    });
 
+
+});
+Route::group(['prefix' => 'new-service-pages'], function () {
+    Route::post('get',[ServicePagesController::class,'getPages']);
+    Route::post('validate-slug',[ServicePagesController::class,'validateSlug']);
+    Route::post('add',[ServicePagesController::class,'store']);
+    Route::get('delete-page/{id}',[ServicePagesController::class,'delete_page']);
 });
