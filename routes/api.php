026d5a0e6aabf326abe232247b94f715aedde56d
saifali727 <saifalich2@gmail.com>
2025-03-11 16:16:46 +0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DesignServiceController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServiceController;
@@ -11,3 +12,8 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/service-plans', [DesignServiceController::class, 'getServicePlans']);
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/send-otp', [AuthController::class, 'sendOtp']);
+Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
