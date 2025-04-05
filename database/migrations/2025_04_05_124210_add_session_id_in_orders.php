89d230852a04ad9ade826a7f4ebbb9d5a3eaf4ad
saifalich2 <saifalich2@gmail.com>
2025-04-05 17:44:03 +0500

    changes

---
+++ database/migrations/2025_04_05_124210_add_session_id_in_orders.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('session_id')->nullable()->after('price');
+            $table->string('paid_amount')->nullable()->after('session_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('session_id');
+            $table->dropColumn('paid_amount');
+        });
+    }
+};
