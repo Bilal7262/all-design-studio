14213d1065fe60af9d485f4a3e444876c3431fef
Bilal786 <bilalbashir95@gmail.com>
2025-05-16 13:57:32 +0500

    changes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
             $validator = Validator::make($request->all(), [
                 'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
-                'phone' => 'required|string|max:15|unique:users',
+                'phone' => 'required|string|max:20|unique:users',
                 'password' => 'required|string|min:8',
                 'confirm_password' => 'required|string|same:password',
             ],[
